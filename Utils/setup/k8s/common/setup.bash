f07d5c0c7707cb0ffbb27cc1196cd392e12ea353
moelang <moelang@hotmail.com>
2021-05-17 18:35:11 +0800

    ✨ feat(k8s): config net bridge

--- Utils/setup/k8s/common/setup.bash
+++ Utils/setup/k8s/common/setup.bash
@@ -16,3 +16,14 @@ source $CURDIR/minikube/setup.bash
 source $CURDIR/helm/setup.bash
 
 source $CURDIR/docker/setup.bash
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+sudo sysctl --system
